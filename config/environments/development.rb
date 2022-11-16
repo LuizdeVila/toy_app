b5df9d8c48b353d2497ec58dd18d0a8e68a9ee2a
Luizdevila <luizdevila1@icloud.com>
2022-11-16 16:25:12 -0300

    Finish toy app

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -60,3 +60,9 @@ Rails.application.configure do
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
 end
+
+Rails.application.configure do
+  # Allow Cloud9 connections.
+  config.hosts.clear
+end
+
